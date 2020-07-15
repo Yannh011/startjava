6f53804e125455ac6f5d1f23d54e12636b7c0770
Yannh011 <yannh0101@gmail.com>
2020-07-15 16:16:01 +0300

    implement HW_1

--- lesson_1/Unicode.java
+++ lesson_1/Unicode.java
@@ -1,11 +1,9 @@
-import java.util.*;
 public class Unicode {
 	public static void main(String[] args) {
-	Random rand_char = new Random();
-	int number = 12615;
-	int unicode = rand_char.nextInt(number) + 10111;
-	char smile = (char)(unicode);
-	System.out.println(unicode);
-	System.out.println(smile);
+	char i = 33;
+		while (i <= 126){
+			System.out.println(i);
+			i++;
+		}
 	}
 }
