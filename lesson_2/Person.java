df8db70a8a616c8c32d73077b3d2e0149afd9e7d
Yannh011 <yannh0101@gmail.com>
2020-07-26 17:34:12 +0300

    implement HW_1

---
+++ lesson_2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+	
+	String name = "Dasha";
+	String sex = "Female";
+	int height = 165;
+	int weight = 55;
+	int age = 25;
+
+	void walk() {
+
+	}
+	void sit() {
+
+	}
+	void run() {
+
+	}
+	void speak() {
+
+	}
+	void learnJava() {
+
+	}
+}
