900d818bea86d193746334ba04587f234bad8d98
Yannh011 <yannh0101@gmail.com>
2020-07-30 22:42:30 +0300

    implement HW_1

--- lesson_2/Calculator.java
+++ lesson_2/Calculator.java
@@ -1,37 +1,54 @@
 public class Calculator {
 
-	int a;
-	int b;
-	char sign;
-	int result;
+	private int num1;
+	private int num2;
+	private char sign;
 
-	switch (sign) {
-		case '+':
-		result = a + b;
-		break;
+	public int getNum1() {
+		return num1;
+	}
+	void setA(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	void setB (int num2) {
+		this.num2 = num2;
+	}
+	public char getSign() {
+		return sign;
+	}
+	void setSign(char sign) {
+		this.sign = sign;
+	}
 
-		case '/':
-		result = a / b;
-		break;
-
-		case '+':
-		result = a + b;
-		break;
-
-		case '*':
-		result = a * b;
-		break;
-
-		case '%':
-		result = a % b;
-		break;
-
-		case '^':
-			int result1 = 1;
-			for (int i = 0; i < b; i++) {
-			result1 *= a;
-			}
-			System.out.println(result1);
+	public static int calc(int num1, int num2, char sign) {
+		int result;
+		switch (sign) {
+			case '+':
+			result = num1 + num2;
 			break;
+			case '/':
+			result = num1 / num2;
+			break;
+			case '-':
+			result = num1 - num2;
+			break;
+			case '*':
+			result = num1 * num2;
+			break;
+			case '%':
+			result = num1 % num2;
+			break;
+			case '^':
+				int result1 = 1;
+				for (int i = 0; i < num2; i++) {
+				result1 *= num1;
+				}
+				System.out.println(result1);
+				break;
+		}
+	return result;
 	}
 }
--- lesson_2/CalculatorTest.java
+++ lesson_2/CalculatorTest.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+
 public class CalculatorTest {
 	public static void main(String[] args) {
-
+	Calculator calculatorOne = new Calculator();
 		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		char sign = scanner.next().charAt(0);
-		int b = scanner.nextInt();
-		System.out.println();
+		calculatorOne getNum1() = scanner.nextInt();
+		calculatorOne.getNum2() = scanner.nextInt();
+		calculatorOne.getSign() = scanner.next().charAt(0);
+		int result = calc(num1,num2,sign);
+		System.out.println(result);
 	}
 }
