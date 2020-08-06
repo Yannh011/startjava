f9648e77267e3a88ec65c53ad816c76b941cb0ae
Yannh011 <yannh0101@gmail.com>
2020-08-06 15:43:56 +0300

    implement HW_1

--- lesson_2/Jaeger.java
+++ lesson_2/Jaeger.java
@@ -12,6 +12,10 @@ public class Jaeger {
 		this.kaijuKilled = kaijuKilled; 
 	}
 
+	public String toString() {
+		return this.modelName + " " + this.height + " " + this.weight + " " + this.kaijuKilled;
+	}
+
 	public String getModelName() {
 		return modelName;
 	}
