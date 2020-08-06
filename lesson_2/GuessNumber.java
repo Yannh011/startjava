59179069af4256beea14727aab4a988b808b5093
Yannh011 <yannh0101@gmail.com>
2020-08-06 19:54:03 +0300

    implement HW_1

---
+++ lesson_2/GuessNumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+
+	Scanner scan = new Scanner(System.in);
+	private Player player1;
+	private Player player2;
+
+	public GuessNumber(Player player1, Player player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+
+	public void startGame() {
+		int numberQuiz = (int) ( Math.random() * 100);
+		do { 
+			player1.setNumber(scan.nextInt());
+			if (player1.getNumber() < numberQuiz) {
+				System.out.println("Загаданное число больше");
+			} else if (player1.getNumber() > numberQuiz) {
+				System.out.println("Загаданное число меньше");
+			} else if (player1.getNumber() == numberQuiz) {
+				System.out.println("Victory" + player1.getName());
+			}
+			break;
+
+			player2.setNumber(scan.nextInt());           // ЗДЕСЬ ВЫДАЕТ ОШИБКУ.
+			if (player2.getNumber() < numberQuiz) {
+				System.out.println("Загаданное число больше");
+			} else if (player2.getNumber() > numberQuiz) {
+				System.out.println("Загаданное число меньше");
+			} else if (player2.getNumber() == numberQuiz) {
+				System.out.println("Victory" + player2.getName());
+			}
+			break; 
+		} while (true);
+	}
+
+
+}
---
+++ lesson_2/Player.java
@@ -0,0 +1,20 @@
+public class Player {
+	private String name;
+	private int number;
+
+	public String getName() {
+		return name;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	void setNumber(int number) {
+		this.number = number;
+	}
+}
